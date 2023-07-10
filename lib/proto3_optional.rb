98cff73d92276a046b54e2a7228eb8443db72c89
Samuel Oyediran <samuel.oyediran@shopify.com>
2023-07-10 16:31:45 -0400

    proto3 optional field work the same way google wrapper works but with some caveats

--- lib/proto3_optional.rb
+++ lib/proto3_optional.rb
@@ -6,14 +6,33 @@ require 'google/protobuf'
 require_relative "../protobuf/load_proto"
 require "debug"
 
+class Task
+  extend T::Sig
 
-extend T::Sig
+  sig do
+    params(
+      id: T.nilable(Integer),
+      name: T.nilable(String),
+      completed: T.nilable(T::Boolean),
+    ).void
+  end
+  def initialize(id:, name:, completed:)
+    @id = id
+    @name = name
+    @completed = completed
+  end
+end
 
 task = Protobuf::Task.new(id: 1, name: nil, completed: nil)
 
 encoded_task = Protobuf::Task.encode(task)
 decoded_task = Protobuf::Task.decode(encoded_task)
 
+new_task = Task.new(
+  id: decoded_task.has_id? ? decoded_task.id : nil,
+  name: decoded_task.has_name? ? decoded_task.name : nil,
+  completed: decoded_task.has_completed? ? decoded_task.completed : nil,
+)
 
 country = Protobuf::Country.new(
   id: 456,
