f1d81fb948c42bdb93968af747f5a028e134ce34
Samuel Oyediran <samuel.oyediran@shopify.com>
2023-07-17 16:24:10 -0400

    using oneof in protobuf

---
+++ lib/oneof_proto.rb
@@ -0,0 +1,12 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "sorbet-runtime"
+require 'google/protobuf'
+require_relative "../protobuf/load_proto"
+require "debug"
+
+options = Protobuf::Optional.new
+
+
+puts "test"
